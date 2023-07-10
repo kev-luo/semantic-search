d42224bdda78ee099a29717b870c04a47bb3a86d
Kevin Luo <kvn.luo@gmail.com>
2023-07-10 08:07:09 -0500

    function to upload data to pinecone

--- utils.ts
+++ utils.ts
@@ -5,10 +5,8 @@ import { loadQAStuffChain } from 'langchain/chains';
 import { Document } from 'langchain/document';
 import { timeout } from './config';
 
-// fxn for querying pinecone once we've stored data there
-
 // fxn for creating pinecone index
-export const createPineConeIndex = async (client, indexName, vectorDimension) => {
+export const createPineconeIndex = async (client, indexName, vectorDimension) => {
     // initialize index existence check
     console.log(`Checking "${indexName}"...`);
     // get list of existing indexes that are available
@@ -36,3 +34,56 @@ export const createPineConeIndex = async (client, indexName, vectorDimension) =>
 }
 
 // fxn for uploading data to the pinecone index
+export const updatePinecone = async (client, indexName, docs) => {
+    // retrieve pinecone index
+    const index = client.Index(indexName);
+    // log the retrieved index name
+    console.log(`Pinecone index retrieved: ${indexName}`);
+    // map over docs 
+    for (const doc of docs) {
+        // retrieve some doc metadata
+        console.log(`Processing document: ${doc.metadata.source}`);
+        const txtPath = doc.metadata.source;
+        const text = doc.pageContent;
+        // create instance of Text Splitter
+        const textSplitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 1000,
+        });
+        console.log("Splitting text into chunks");
+        // split text into chunks (documents)
+        const chunks = await textSplitter.createDocuments([text]);
+        console.log(`Text split into ${chunks.length} chunks`);
+        // call openai's embedding endpoint & create openai embeddings for documents
+        const embeddingsArrays = await new OpenAIEmbeddings().embedDocuments(
+            chunks.map((chunk) => chunk.pageContent.replace(/\n/g, " "))
+        );
+        // create and upsert vectors in batches of 100
+        const batchSize = 100;
+        let batch: any = [];
+        for (let idx = 0; idx < chunks.length; idx++) {
+            const chunk = chunks[idx];
+            const vector = {
+                id: `${txtPath}_${idx}`,
+                values: embeddingsArrays[idx],
+                metadata: {
+                    ...chunk.metadata,
+                    loc: JSON.stringify(chunk.metadata.loc),
+                    pageContent: chunk.pageContent,
+                    txtPath: txtPath,
+                },
+            };
+            batch = [...batch, vector]
+            // when batch is full or it's the last item, upsert the vectors
+            if (batch.length === 100 || idx === chunks.length - 1) {
+                await index.upsert({
+                    upsertRequest: {
+                        vectors: batch,
+                    },
+                });
+                batch = [];
+            }
+        }
+    }
+}
+
+// fxn for querying pinecone once we've stored data there
